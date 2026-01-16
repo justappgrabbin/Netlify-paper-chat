8c13a0177255b55ec2e7cc32dc514633fb7a7827
justappgrabbin <bookingalexismcmillion@gmail.com>
2026-01-16 01:41:30 -0800

    Delete chat.js

--- chat.js
+++
@@ -1,9 +0,0 @@
-export async function sendMessage(messages) {
-  const res = await fetch("/.netlify/functions/chat", {
-    method: "POST",
-    body: JSON.stringify({ messages })
-  });
-
-  const json = await res.json();
-  return json.choices[0].message.content;
-}
